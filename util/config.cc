01345aa069d5e9244ce058ffeddd4b7557bba3d2
Viacheslav Galaktionov <viacheslav.galaktionov@gmail.com>
2018-12-04 23:04:05 +0300

    Add missing files

---
+++ util/config.cc
@@ -0,0 +1,8 @@
+#include "config.h"
+
+namespace ToyDBMS {
+    namespace Config {
+        size_t available_memory = 0;
+        size_t block_size = 0;
+    }
+}
---
+++ util/config.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstddef>
+
+namespace ToyDBMS {
+    namespace Config {
+        extern size_t available_memory;
+        extern size_t block_size;
+    };
+}
